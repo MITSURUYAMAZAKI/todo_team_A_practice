40e04f4192a5cfecd2dd622aa8b5d1b0e2fbe8ba
sakusakuhongoroo <80045643+sakusakuhongoroo@users.noreply.github.com>
2021-07-04 19:50:52 +0800

    Revert back

--- store.php
+++ store.php
@@ -1,15 +1,2 @@
 <?php
-// read the file
-require_once('./models/task.php');
 
-//receive file
-$title = $_POST['title'];
-$contents = $_POST['contents'];
-$timestamp = time("Y年m月d日 H時i分s秒");
-
-// save to DB
-$task = new Task(); 
-$task->create([$title,$contents,$timestamp]);
-
-// redirect to index.php
-header('location:index.php');
