5d0d8b1a09d3b3871dc1ec92040a56c76e345927
sakusakuhongoroo <80045643+sakusakuhongoroo@users.noreply.github.com>
2021-07-06 14:00:21 +0800

    時間の修正

--- store.php
+++ store.php
@@ -6,7 +6,7 @@ require_once('./models/task.php');
 //receive file
 $title = $_POST['title'];
 $contents = $_POST['contents'];
-$timestamp = time("Y年m月d日 H時i分s秒");
+$timestamp = date("Y/m/d H:i:s");
 
 // save to DB
 $task = new Task(); 
