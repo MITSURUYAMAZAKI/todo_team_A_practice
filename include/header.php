12ec8fcbfd0ef2c455b1662d3e3fe41b5c1e8349
山崎満 <zakiyama.com12.aaa@gmail.com>
2021-07-09 13:34:35 +0900

    ナビゲーション追加

--- include/header.php
+++ include/header.php
@@ -1,11 +1,21 @@
 <header>
     <span class="header-left">ToDo_team_A</span>
-    <ul>
-        <li><a href="#">Create</a></li>
-        <li><a href="#">sign in</a></li>
-        <li><form class="form-inline">
-            <input class="form" type="search" placeholder="word" aria-label="Search" name="title">
-            <button class="search" type="submit">Search</button>
-        </form></li>
-    </ul>
+    <button type="button" class="btn js-btn">
+        <span class="btn-line">≡</span>
+    </button>
+    <div class="for_pc" id="drawer_toggle">
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+    <nav class="for_phone" id="global_nav">
+        <ul>
+            <li><a href="#">sign in</a></li>
+            <li><a href="#">Create</a></li>
+            <li><form class="form-inline">
+                <input class="form" type="search" placeholder="word" aria-label="Search" name="title">
+                <button class="search" type="submit">Search</button>
+            </form></li>
+        </ul>
+    </nav>
 </header>
